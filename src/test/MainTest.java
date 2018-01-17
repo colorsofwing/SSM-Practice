881495d367579b12baea7bac120757170b66e69d
colorsofwing <1457633831@qq.com>
2018-01-17 22:47:30 +0800

    pagination

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -9,12 +9,13 @@ import java.util.List;
 
 public class MainTest {
     public static void main(String arg[]){
-        Page page=new Page(0,5);
-        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring-mybatis.xml");
+        Page page=new Page();//实例已经使用默认构造函数初始化
+        System.out.print(page.getParam());
+        /*ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring-mybatis.xml");
         CategoryDaoImpl service = (CategoryDaoImpl) ac.getBean("c");
         List<Category> list = service.list(page);
         for(Category category:list){
             System.out.println(category.getId()+" "+category.getName());
-        }
+        }*/
     }
 }
